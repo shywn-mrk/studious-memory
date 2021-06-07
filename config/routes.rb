6ac5b20ed6b0a73493a0065ee2a55ec23eafaa4f
Shayan Karimi <shayankarimi0078@gmail.com>
2021-06-07 12:12:42 +0430

    Rails scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   post 'authenticate', to: 'authentication#authenticate'
   resources :todos, only: [:create, :index, :show, :update, :destroy,]
+  resources :products
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_06_060804) do
+ActiveRecord::Schema.define(version: 2021_06_07_074017) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "todos", force: :cascade do |t|
     t.string "text"
